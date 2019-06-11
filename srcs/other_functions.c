0fb046bbe0fa419715a68354631295be034e52d8
sschmele <sschmele@oa-j4.21-school.ru>
2019-06-11 18:09:25 +0300

    Ft_strs_rejoin

--- srcs/other_functions.c
+++ srcs/other_functions.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   other_functions.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jcorwin <jcorwin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sschmele <sschmele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 19:08:49 by sschmele          #+#    #+#             */
-/*   Updated: 2019/06/08 15:43:42 by jcorwin          ###   ########.fr       */
+/*   Updated: 2019/06/11 18:07:34 by sschmele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,54 @@ void		ft_stradd_nb(char *s, long nb)
 	}
 }
 
+char        *ft_strs_rejoin(char *main, char **arr, int size) // new
+{
+    char    *res;
+    int     len;
+    int     i;
+
+    if (!main && !arr && size == 0)
+        return (NULL);
+    i = 0;
+    len = 0;
+    while (i < size)
+    {
+        len += ft_strlen(arr[i]);
+        i++;
+    }
+    if (!(res = (char*)malloc(len + 1)))
+        return (NULL);
+    i = 0;
+    while (i < size)
+    {
+        ft_strcat(res, arr[i]);
+        i++;
+    }
+    ft_strdel(&main);
+    return (res);
+}
+
 int			file_strcmp(t_file *left, t_file *right)
 {
 	return (ft_strcmp(left->name, right->name));
+}
+
+void		fill_type(t_file *file, int *flags)
+{
+	if (S_ISDIR(file->info.st_mode))
+		file->type = 'd';
+	else if (S_ISBLK(file->info.st_mode))
+		file->type = 'b';
+	else if (S_ISCHR(file->info.st_mode))
+		file->type = 'c';
+	else if (S_ISFIFO(file->info.st_mode))
+		file->type = 'p';
+	else if (S_ISLNK(file->info.st_mode))
+		file->type = 'l';
+	else if (S_ISREG(file->info.st_mode))
+		file->type = '-';
+	else if (S_ISSOCK(file->info.st_mode))
+		file->type = 's';
+	if (file->type == 'b' || file->type == 'c')
+		*flags |= FLAG_DEVICE;
 }
